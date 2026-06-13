6cb4af67477cd927201a7ef9ed68bc74e06dd78b
hiroto iyonaga <k23rs021@st.kyusan-u.ac.jp>
2026-06-13 15:24:35 +0900

    new

--- Monday.4/Monday.4/RootTabView.swift
+++ Monday.4/Monday.4/RootTabView.swift
@@ -2,15 +2,11 @@ import SwiftUI
 
 struct RootTabView: View {
     var body: some View {
-        
-        
         TabView {
             WorkoutView()
                 .tabItem { Label("筋トレ記録", systemImage: "dumbbell") }
             InBodyScanView()
                 .tabItem { Label("InBody", systemImage: "camera.viewfinder") }
-            CalendarView()
-                .tabItem { Label("カレンダー", systemImage: "calendar") }
             GymMapView()
                 .tabItem { Label("ジムマップ", systemImage: "map") }
         }
